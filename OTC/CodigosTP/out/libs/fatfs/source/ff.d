7c43e62bc70ffdbbc2729eb388425473abbf7e69
Gustavo <lu2jgp@gmail.com>
2021-10-19 10:18:46 -0300

    OTC 1ras config.
    Primeras configuraciones para el proyecto para OTC. Subcarpeta con copia de los .src y modifcacion del Copyright.

---
+++ OTC/CodigosTP/out/libs/fatfs/source/ff.d
@@ -0,0 +1,4 @@
+examples/c/sapi/TP_PCSE_GustavoParedes/CodigosTP/out/libs/fatfs/source/ff.o: \
+ libs/fatfs/source/ff.c libs/fatfs/source/ff.h \
+ libs/fatfs/source/integer.h libs/lpc_fatfs_disks/source/ffconf.h \
+ libs/fatfs/source/diskio.h
